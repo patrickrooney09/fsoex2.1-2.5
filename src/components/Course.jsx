53202a3c39781987780ede5cc89558dc4a988ecf
Patrick Rooney <patrickrooney09@gmail.com>
2023-09-12 16:43:05 -0400

    exercise 2.2 complete

--- src/components/Course.jsx
+++ src/components/Course.jsx
@@ -1,12 +1,20 @@
 import Content from "./Content";
 import Header from "./Header";
+import Total from "./Total";
 
 function Course({ course }) {
   const { id, name, parts } = course;
+
+  const sum = parts.reduce((accumulator, currentElement) => {
+    const number = currentElement.exercises;
+    return accumulator + number;
+  }, 0);
+
   return (
     <div>
       <Header name={name} />
       <Content parts={parts} id={id} />
+      <Total sum={sum} />
     </div>
   );
 }
--- src/components/Total.jsx
+++ src/components/Total.jsx
@@ -1,3 +1,7 @@
-const Total = ({ sum }) => <p>Number of exercises {sum}</p>;
+const Total = ({ sum }) => (
+  <p>
+    <strong>total of {sum} exercises </strong>
+  </p>
+);
 
 export default Total;
